54e44bf710557aac21a0b91cf051da81b6dc9ca0
Sai-Santhan <dsaisanthan@gmail.com>
2022-11-22 11:40:57 +0530

    Added custom Data Types and Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css"
+import {Routes, Route, Navigate} from "react-router-dom";
+import {Container} from "react-bootstrap";
+import {NewNote} from "./NewNote";
+
+export type Note = {
+    id: string
+} & NoteData
+
+export type NoteData = {
+    title: string
+    markdown: string
+    tags: Tag[]
+}
+
+export type Tag = {
+    id: string
+    label: string
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Container className="my-4">
+            <Routes>
+                <Route path="/" element={<h1>Hi from /</h1>}/>
+                <Route path="/new" element={<NewNote/>}/>
+                <Route path="/:id">
+                    <Route index element={<h1>Hi from Show id</h1>}/>
+                    <Route path="edit" element={<h1>Hi from Edit</h1>}/>
+                </Route>
+                <Route path="*" element={<Navigate to="/"/>}/>
+            </Routes>
+        </Container>
+    );
 }
 
 export default App;
